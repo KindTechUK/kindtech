df7be25f824a1c0fc0ab17084a866c8e29edc776
Billy Zhao <billyzhaoyh@gmail.com>
2025-07-07 18:32:53 +0100

    add tests

--- examples/download_nomis_table.py
+++ examples/download_nomis_table.py
@@ -2,3 +2,12 @@ from kindtech.nomis import get_ons_table
 
 table = get_ons_table("NM_1_1")
 print(table)
+geo_table = get_ons_table(
+    "NM_1_1",
+    geography="TYPE480",
+    time="latest",
+    measures=20100,
+    item=1,
+    select=["geography_name", "sex_name", "obs_value"],
+)
+print(geo_table)
